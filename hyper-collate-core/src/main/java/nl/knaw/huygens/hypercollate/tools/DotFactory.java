c3ffd19855282d671e0f7a8220fc1d770c04775e
Bram Buitendijk <bram.buitendijk@huygens.knaw.nl>
2017-11-09 15:23:43 +0100

    [COL-56] formatting

--- hyper-collate-core/src/main/java/nl/knaw/huygens/hypercollate/tools/DotFactory.java
+++ hyper-collate-core/src/main/java/nl/knaw/huygens/hypercollate/tools/DotFactory.java
@@ -1,5 +1,25 @@
 package nl.knaw.huygens.hypercollate.tools;
 
+/*-
+ * #%L
+ * hyper-collate-core
+ * =======
+ * Copyright (C) 2017 Huygens ING (KNAW)
+ * =======
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
 import static java.util.stream.Collectors.joining;
 import static java.util.stream.Collectors.toList;
 
@@ -23,26 +43,6 @@ import nl.knaw.huygens.hypercollate.model.SimpleTokenVertex;
 import nl.knaw.huygens.hypercollate.model.StartTokenVertex;
 import nl.knaw.huygens.hypercollate.model.TokenVertex;
 import nl.knaw.huygens.hypercollate.model.VariantWitnessGraph;
-
-/*-
- * #%L
- * hyper-collate-core
- * =======
- * Copyright (C) 2017 Huygens ING (KNAW)
- * =======
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- *      http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * #L%
- */
 
 public class DotFactory {
 
@@ -134,14 +134,8 @@ public class DotFactory {
             Node source = collation.getSource(e);
             Node target = collation.getTarget(e);
             String edgeLabel = e.getSigils().stream().sorted().collect(joining(","));
-            String lineBuilder = //
-                nodeIdentifiers.get(source) + //
-            "->" + //
-            nodeIdentifiers.get(target) + //
-            "[label=\"" + //
-            edgeLabel + //
-            "\"]\n";
-            edgeLines.add(lineBuilder);
+            String line = nodeIdentifiers.get(source) + "->" + nodeIdentifiers.get(target) + "[label=\"" + edgeLabel + "\"]\n";
+            edgeLines.add(line);
           });
     }
     edgeLines.forEach(dotBuilder::append);
