baa90b2bdc011a4e46c788e046e45e213337e4c0
Bram Buitendijk <bram.buitendijk@huygens.knaw.nl>
2018-01-16 13:10:41 +0100

    Merge branch 'feature-col-61' into develop
    # Conflicts:
    #	hyper-collate-core/src/test/java/nl/knaw/huygens/hypercollate/collator/HyperCollatorTest.java

--- hyper-collate-core/src/test/java/nl/knaw/huygens/hypercollate/collator/HyperCollatorTest.java
+++ hyper-collate-core/src/test/java/nl/knaw/huygens/hypercollate/collator/HyperCollatorTest.java
@@ -1,45 +1,5 @@
 package nl.knaw.huygens.hypercollate.collator;
 
-import static java.util.Arrays.asList;
-import static java.util.stream.Collectors.joining;
-import static java.util.stream.Collectors.toList;
-import static java.util.stream.Collectors.toSet;
-import static org.assertj.core.api.Assertions.assertThat;
-
-import java.text.MessageFormat;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import java.util.concurrent.TimeUnit;
-
-import org.assertj.core.api.Assertions;
-import org.assertj.core.util.Sets;
-import org.junit.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-/*-
- * #%L
- * hyper-collate-core
- * =======
- * Copyright (C) 2017 - 2018 Huygens ING (KNAW)
- * =======
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- *      http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * #L%
- */
-
 import com.google.common.base.Stopwatch;
 import eu.interedition.collatex.dekker.Tuple;
 import nl.knaw.huygens.hypercollate.HyperCollateTest;
@@ -50,6 +10,7 @@ import nl.knaw.huygens.hypercollate.model.TokenVertex;
 import nl.knaw.huygens.hypercollate.model.VariantWitnessGraph;
 import nl.knaw.huygens.hypercollate.tools.CollationGraphNodeJoiner;
 import nl.knaw.huygens.hypercollate.tools.CollationGraphVisualizer;
+import org.assertj.core.api.Assertions;
 import org.assertj.core.util.Sets;
 import org.junit.Ignore;
 import org.junit.Test;
@@ -63,6 +24,26 @@ import java.util.concurrent.TimeUnit;
 import static java.util.Arrays.asList;
 import static java.util.stream.Collectors.*;
 import static org.assertj.core.api.Assertions.assertThat;
+
+/*-
+ * #%L
+ * hyper-collate-core
+ * =======
+ * Copyright (C) 2017 - 2018 Huygens ING (KNAW)
+ * =======
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
 
 public class HyperCollatorTest extends HyperCollateTest {
   private static final Logger LOG = LoggerFactory.getLogger(HyperCollateTest.class);
