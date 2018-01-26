531be1b7b02675bdbe5af061d3f52a1ee494bcf5
Bram Buitendijk <bram.buitendijk@huygens.knaw.nl>
2018-01-26 15:21:49 +0100

    [COL-70] Refactor REST API, upload witness xml one witness at a time.

--- hyper-collate-server/src/main/java/nl/knaw/huygens/hypercollate/dropwizard/resources/CollationsResource.java
+++ hyper-collate-server/src/main/java/nl/knaw/huygens/hypercollate/dropwizard/resources/CollationsResource.java
@@ -1,5 +1,25 @@
 package nl.knaw.huygens.hypercollate.dropwizard.resources;
 
+/*-
+ * #%L
+ * hyper-collate-server
+ * =======
+ * Copyright (C) 2017 - 2018 Huygens ING (KNAW)
+ * =======
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
 import com.codahale.metrics.annotation.Timed;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -25,26 +45,6 @@ import javax.ws.rs.core.Response;
 import java.net.URI;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
-
-/*-
- * #%L
- * hyper-collate-server
- * =======
- * Copyright (C) 2017 - 2018 Huygens ING (KNAW)
- * =======
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * #L%
- */
 
 @Api(ResourcePaths.COLLATIONS)
 @Path(ResourcePaths.COLLATIONS)
@@ -105,7 +105,7 @@ public class CollationsResource {
   @Consumes(UTF8MediaType.TEXT_XML)
   @ApiOperation(value = "Add a witness to the collation")
   public Response addXMLWitness(@ApiParam(APIPARAM_NAME) @PathParam("name") final String name, @ApiParam(APIPARAM_SIGIL) @PathParam("sigil") final String sigil,
-      @ApiParam(APIPARAM_XML) @NotNull @Valid String xml) {
+                                @ApiParam(APIPARAM_XML) @NotNull @Valid String xml) {
     CollationInfo collationInfo = getExistingCollationInfo(name);
     collationInfo.addWitness(sigil, xml);
     collationStore.persist();
