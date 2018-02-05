0cd01f16e63e31e3467588a6db425f50adcfe22e
Bram Buitendijk <bram.buitendijk@huygens.knaw.nl>
2018-02-05 10:24:53 +0100

    Update copyright headers

--- hyper-collate-server/src/main/java/nl/knaw/huygens/hypercollate/dropwizard/resources/CollationsResource.java
+++ hyper-collate-server/src/main/java/nl/knaw/huygens/hypercollate/dropwizard/resources/CollationsResource.java
@@ -1,5 +1,24 @@
 package nl.knaw.huygens.hypercollate.dropwizard.resources;
 
+/*-
+ * #%L
+ * hyper-collate-server
+ * =======
+ * Copyright (C) 2017 - 2018 Huygens ING (KNAW)
+ * =======
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
 import com.codahale.metrics.annotation.Timed;
 import com.google.common.base.Stopwatch;
 import io.swagger.annotations.Api;
@@ -25,26 +44,6 @@ import javax.ws.rs.core.Response;
 import java.net.URI;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
-
-/*-
- * #%L
- * hyper-collate-server
- * =======
- * Copyright (C) 2017 - 2018 Huygens ING (KNAW)
- * =======
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * #L%
- */
 
 @Api(ResourcePaths.COLLATIONS)
 @Path(ResourcePaths.COLLATIONS)
