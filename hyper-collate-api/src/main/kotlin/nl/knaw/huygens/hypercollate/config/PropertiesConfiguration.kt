5297df292fb347ab540dbc3c8290f6b9bb1ef6a2
Bram Buitendijk <bram.buitendijk@di.huc.knaw.nl>
2021-07-28 16:46:51 +0200

    update copyright headers

--- hyper-collate-api/src/main/kotlin/nl/knaw/huygens/hypercollate/config/PropertiesConfiguration.kt
+++ hyper-collate-api/src/main/kotlin/nl/knaw/huygens/hypercollate/config/PropertiesConfiguration.kt
@@ -4,7 +4,7 @@ package nl.knaw.huygens.hypercollate.config
  * #%L
  * hyper-collate-api
  * =======
- * Copyright (C) 2017 - 2020 Huygens ING (KNAW)
+ * Copyright (C) 2017 - 2021 Huygens ING (KNAW)
  * =======
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
